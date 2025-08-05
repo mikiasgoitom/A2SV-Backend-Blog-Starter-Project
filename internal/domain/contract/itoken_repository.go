e67aaaf4e5fdcc7dceee1c7e0694b7cef8aa8326
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-08-05 14:52:07 +0300

    fix(token_repo): rename Create method to CreateToken for consistency

--- internal/domain/contract/itoken_repository.go
+++ internal/domain/contract/itoken_repository.go
@@ -6,7 +6,7 @@ import (
 )
 
 type ITokenRepository interface {
-	Create(ctx context.Context, token *entity.Token) error
+	CreateToken(ctx context.Context, token *entity.Token) error
 	GetByID(ctx context.Context, id string) (*entity.Token, error)
 	GetByUserID(ctx context.Context, userID string) (*entity.Token, error)
 	Revoke(ctx context.Context, id string) error
